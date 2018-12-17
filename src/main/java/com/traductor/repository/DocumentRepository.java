58ea0f0f809023a68d7d9a8e9531b596fb27fc02
Carlos Madrigal <cmadrigaln@ucenfotec.ac.cr>
2018-12-16 23:02:59 -0600

    Amazon Translate and S3 services added

--- src/main/java/com/traductor/repository/DocumentRepository.java
+++ src/main/java/com/traductor/repository/DocumentRepository.java
@@ -4,4 +4,6 @@ import com.traductor.domain.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+
 }
