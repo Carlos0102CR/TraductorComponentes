78507e5a18b2e2138eb8aa9c424664d117696c83
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-12-16 22:53:20 -0600

    Added log-in logic

--- src/main/java/com/traductor/repository/UserRepository.java
+++ src/main/java/com/traductor/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.traductor.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	User findByUsernameAndPassword(String username, String password);
 }
