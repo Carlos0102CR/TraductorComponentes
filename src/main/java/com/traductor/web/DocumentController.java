80a3088a66e3c25a26563aee1c3413fa0b7fcbec
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-12-19 01:40:04 -0600

    Added logic for user document list

--- src/main/java/com/traductor/web/DocumentController.java
+++ src/main/java/com/traductor/web/DocumentController.java
@@ -1,15 +1,17 @@
 package com.traductor.web;
 
+
 import com.traductor.aws.AmazonClient;
 import com.traductor.domain.Document;
-import com.traductor.domain.Translate;
+import com.traductor.domain.User;
 import com.traductor.repository.DocumentRepository;
+import com.traductor.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RequestMapping("/document")
 @Controller
@@ -17,6 +19,8 @@ public class DocumentController {
 
     @Autowired
     DocumentRepository repo;
+    @Autowired
+    UserRepository usRepo;
 
     @Autowired
     AmazonClient AWSClient;
@@ -25,13 +29,31 @@ public class DocumentController {
     @ResponseBody
     public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
         return this.AWSClient.uploadFile(file);
-    }*/
+    }*/  
 
     @GetMapping("/{id}")
     public String listDocuments(@PathVariable Long id,Model model) {
+    	
+    	List<Document> documentList;
+		User user = usRepo.findById(id).get();
+		model.addAttribute("user", user);
+		documentList = obtainDocumentsOfUser(id);
+		model.addAttribute("documents", documentList);
 
-        model.addAttribute("documents",repo.findAllByIdUser(id));
-
-        return "documents";
+		return "documents";
     }
+    
+    private List<Document> obtainDocumentsOfUser(Long idUser) {
+		List<Document> documentList = new ArrayList<>();
+		List<Document> allDocuments;
+		allDocuments = repo.findAll();
+
+		for (Document document : allDocuments) {
+			if (document.getIdUser() == idUser) {
+				documentList.add(document);				
+			}
+		}
+
+		return documentList;
+	}
 }
