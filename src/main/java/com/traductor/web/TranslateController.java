61b6ca50b9b2455066abd628959e53e50d7f371c
Carlos Madrigal <cmadrigaln@ucenfotec.ac.cr>
2018-12-19 16:10:34 -0600

    added AWSClient constructor in translateController

--- src/main/java/com/traductor/web/TranslateController.java
+++ src/main/java/com/traductor/web/TranslateController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping
 public class TranslateController {
 
-//    AmazonClient AWSClient = new AmazonClient();
+    AmazonClient AWSClient;
 
     @GetMapping
     public String formTranslation(Model model) {
@@ -24,6 +24,7 @@ public class TranslateController {
 
     @PostMapping
     public String submitTranslation(@ModelAttribute("translate") Translate translate,Model model) {
+        AWSClient = new AmazonClient();
 //        model.addAttribute("translate",AWSClient.translate(translate));
         return "index";
     }
