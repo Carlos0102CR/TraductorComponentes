b9b7540416f34a417a11f30f68d0ff27c71d443b
Carlos Madrigal <cmadrigaln@ucenfotec.ac.cr>
2018-12-19 15:49:08 -0600

    aws translate uncomented

--- src/main/java/com/traductor/aws/AmazonClient.java
+++ src/main/java/com/traductor/aws/AmazonClient.java
@@ -29,63 +29,64 @@ import java.util.List;
 
 @Component
 public class AmazonClient {
-//
+    //
 //    private AmazonS3 s3client;
-//    private AmazonTranslate translateClient;
-//    private AmazonComprehend comprehendClient;
-//    private String region = "us-east-2";
-//
+    private AmazonTranslate translateClient;
+    private AmazonComprehend comprehendClient;
+    private String region = "us-east-2";
+    //
 //    @Value("${amazonProperties.endpointUrl}")
 //    private String endpointUrl;
 //    @Value("${amazonProperties.bucketName}")
 //    private String bucketName;
-//    @Value("${amazonProperties.accessKey}")
-//    private String accessKey;
-//    @Value("${amazonProperties.secretKey}")
-//    private String secretKey;
+    @Value("${amazonProperties.accessKey}")
+    private String accessKey;
+    @Value("${amazonProperties.secretKey}")
+    private String secretKey;
+
+    //
+    @PostConstruct
+    private void initializeAmazon() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
 //
-//    @PostConstruct
-//    private void initializeAmazon() {
-//        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-//
-//        this.translateClient = AmazonTranslateClientBuilder
-//                .standard()
-//                .withRegion(region)
-//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-//                .build();
+        this.translateClient = AmazonTranslateClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
 //        this.s3client = AmazonS3ClientBuilder
 //                .standard()
 //                .withRegion(region)
 //                .withCredentials(new AWSStaticCredentialsProvider(credentials))
 //                .build();
-//        this.comprehendClient = AmazonComprehendClientBuilder.standard()
-//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-//                .withRegion(region)
-//                .build();
-//    }
-//
-//    private Language dominantLanguage(String text, Language source) {
-//        DetectDominantLanguageRequest request = new DetectDominantLanguageRequest().withText(text);
-//        DetectDominantLanguageResult result = comprehendClient.detectDominantLanguage(request);
-//        List<DominantLanguage> languages;
-//
-//        languages = result.getLanguages();
-//
-//        source.setLangCode(languages.get(0).getLanguageCode());
-//
-//        return source;
-//    }
-//
-//    public Translate translate(Translate trans) {
-//        TranslateTextResult result;
-//        if (trans.getSourceLanguage().getLangCode().equals("auto")) {
-//            trans.setSourceLanguage(dominantLanguage(trans.getText(), trans.getSourceLanguage()));
-//        }
-//        result = translateClient.translateText(trans.getRequest());
-//        trans.setTranslatedText(result.getTranslatedText());
-//
-//        return trans;
-//    }
+        this.comprehendClient = AmazonComprehendClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+
+    private Language dominantLanguage(String text, Language source) {
+        DetectDominantLanguageRequest request = new DetectDominantLanguageRequest().withText(text);
+        DetectDominantLanguageResult result = comprehendClient.detectDominantLanguage(request);
+        List<DominantLanguage> languages;
+
+        languages = result.getLanguages();
+
+        source.setLangCode(languages.get(0).getLanguageCode());
+
+        return source;
+    }
+
+    public Translate translate(Translate trans) {
+        TranslateTextResult result;
+        if (trans.getSourceLanguage().getLangCode().equals("auto")) {
+            trans.setSourceLanguage(dominantLanguage(trans.getText(), trans.getSourceLanguage()));
+        }
+        result = translateClient.translateText(trans.getRequest());
+        trans.setTranslatedText(result.getTranslatedText());
+
+        return trans;
+    }
 //
 //    private File convertMultiPartToFile(MultipartFile file, Translate trans) throws IOException {
 //        File convFile = new File(file.getOriginalFilename());
